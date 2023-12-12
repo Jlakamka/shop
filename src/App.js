380e49a628b718ae760cbaef046adef75ff0174d
Jlakamka <selov.dmitriy@mail.ru>
2023-12-12 16:42:33 +0300

    3 commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-function App() {
+import Items from "./components/Items";
+
+class App extends React.Component {
+  constructor (props){
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Стул серый',
+          img: 'chair-grey.jpg',
+          desc: 'Lorem',
+          category: 'chairs',
+          price: '49.99'
+        },
+        {
+          id: 2,
+          title: 'Диван черный',
+          img: 'black-sofa.jpg',
+          desc: 'Lorem lorem',
+          category: 'sofas',
+          price: '99.99'
+        }
+      ]
+    }
+  }
+  render(){
   return (
     <div className="wrapper">
       <Header />
+      <Items items={this.state.items}/>
       <Footer />
     </div>
   );
+}
 }
 
 export default App;
